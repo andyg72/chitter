4562d3af5195d6cb58538c4990c11b8ce2076a08
Andy <andygnewman@gmail.com>
2015-01-27 13:12:22 +0000

    refactoring of style and got post peep text to work again html issue

--- models/peep.rb
+++ models/peep.rb
@@ -3,7 +3,7 @@ class Peep
   include DataMapper::Resource
 
   property :id, Serial
-  property :peep_text, Text
+  property :peep_text, String
   property :peep_timestamp, Time
 
   belongs_to :user
